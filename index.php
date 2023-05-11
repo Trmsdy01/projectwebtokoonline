300f1a3d000d2ec4c363194f640f7ffe0aad7d1f
Trmsdy01 <65242744+Trmsdy01@users.noreply.github.com>
2023-05-11 19:13:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,170 @@
+<?php 
+// koneksi ke database
+session_start(); 
+$koneksi = mysqli_connect("localhost", "root", "", "dbtoko"); 
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../projectFinal/css/style.css">
+    <title>Rokajelly Shop</title>
+  </head>
+
+<body style="background-color: #DCDCDC;">
+
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+  <a class="navbar-brand mb-2" href="index.php">
+      <img src="../projectFinal/foto/paw1.png" alt="" width="30" height="30" class="d-inline-block align-text-top me-2">RokaJelly Shop</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active textForm" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link textForm" href="keranjang.php">Cart</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link textForm" href="checkout.php">Checkout</a>
+        </li>
+      </ul>
+               <!--jika sudah login(ada sesssion pelanggan)-->
+               <?php if(isset($_SESSION["pelanggan"])):?>
+            <form class="d-flex me-3">
+                <a class="btn btn-outline-secondary text-light textForm" href="logout.php" role="button">Sign Out</a>
+            </form>
+          <!--jika sudah login(ada sesssion pelanggan)-->
+          <?php else: ?>
+            <form class="d-flex me-3">
+                <a class="btn btn-outline-secondary text-light textForm" href="login.php" role="button">Sign In</a>
+            </form>
+            <?php endif ?>
+    </div>
+  </div>
+</nav>
+<!-- navbar -->
+
+
+<!--carousel-->
+<div class="container">   
+  <div id="carouselExampleControls" class="carousel  mt-5" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="../projectFinal/foto/img1.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../projectFinal/foto/img2.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../projectFinal/foto/img3.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../projectFinal/foto/img4.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../projectFinal/foto/img5.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../projectFinal/foto/img6.png" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+<!--carousel-->
+
+
+<!-- produk -->
+<div class="container mt-4">
+      <div class="judul-kategori" style="background-color: #fff; padding: 5px 10px">
+        <h4 class="text-center" style="font-size: 22px; font-weight: 900;">PRODUK</h4>
+      </div>
+      <div class="row">
+      <div class="container mt-2">
+        <div class="row">
+
+        <?php $ambil=$koneksi->query("SELECT * FROM produk")?>
+        <?php while($perproduk=$ambil->fetch_assoc()){ ?>
+
+          <div class="col-lg-2 col-md-2 col-sm-4 col-6 mt-2">
+          <div class="card">
+            <img src="../projectFinal/admin/fotoproduk/<?php echo $perproduk['foto_produk'] ?>" class="card-img-top img" alt="...">
+            <div class="card-body">
+              <h6 class="card-title text-center textForm"><?php echo $perproduk['nama_produk'] ?></h6>
+              <p class="card-text text-center">Rp. <?php echo number_format($perproduk['harga_produk']); ?></p>
+              <div class="d-grid gap-2">
+              <a href="detail.php" class="btn btn-outline-dark" type="button">Detail</a>
+              <a href="beli.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-dark " type="button">Buy</a>
+              </div>
+            </div>
+          </div>
+        </div>  
+            <?php }?>    
+        </div>
+    </div>
+  </div>
+</div>
+<!-- produk -->
+
+<!-- Footer -->
+<footer class="bg-light p-5 mt-5" >
+  <div class="conteiner">
+    <div class="row">
+      <div class="col-md-6">
+        <a href="" class="text-decoration-none">
+          <img src="../projectFinal/foto/paw1.png" style="width: 30px;" >
+        </a>
+        <span>
+          Copyright @2023 | Created and Develop by <a href="https://www.linkedin.com/in/trie-meutia-5149a8192/" class="text-decoration-none text-dark fw-bold">Trie Meutia Saddyah</a>
+        </span>
+      </div>
+
+      <div class="col-md-6 text-end">
+        <a href="" class="text-decoration-none">
+          <img src="../projectFinal/foto/facebook.png" class="ms-2" style="width: 40px;" >
+        </a>
+        <a href="" class="text-decoration-none">
+          <img src="../projectFinal/foto/linkedin.png" class="ms-2" style="width: 40px;" >
+        </a>
+        <a href="" class="text-decoration-none">
+          <img src="../projectFinal/foto/instagram.png" class="ms-2" style="width: 40px;" >
+        </a>
+        <a href="" class="text-decoration-none">
+          <img src="../projectFinal/foto/twitter.png" class="ms-2" style="width: 40px;" >
+        </a>
+      </div>
+
+    </div>
+  </div>
+</footer>    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,142 @@
+<?php 
+// koneksi ke database
+session_start(); 
+$koneksi = mysqli_connect("localhost", "root", "", "dbtoko"); 
+
+ 
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../projectFinal/css/stylelogin.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pathway+Extreme:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
+    <title>Login</title>
+  </head>
+  <body>
+
+  <!-- login -->
+  <form method="post">
+    <section class="login d-flex">
+        <div class="login-left w-50 h-100">
+            <div class="row justify-content-center align-items-center h-100">
+                <div class="col-6">
+                <div class="header">
+                <h1>Welcome Back</h1>
+                <p>Welcome back! Please enter your details.</p>
+            </div>
+            <div class="login-form">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email"  name="email" class="form-control" id="email" placeholder=" Enter Your Email">
+
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="password" placeholder=" Enter Your Password">
+            <a href="#" class="text-center">Forgot Password</a>
+            <button class="signin" name="login" >Sign In</button>
+
+            <div class="text-center">
+            <span class="d-inline">Don't Have an Account? <a href="#" class="signup d-inline text">Sign Up fo Free</a>
+            </span>
+            </div>
+            </div>
+            </div>
+            </div>
+        </div>
+        <!-- login -->
+
+        <!--carousel-->
+        <div class="login-right w-50 h-100">
+            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="../projectFinal/foto/3.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="../projectFinal/foto/4.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="../projectFinal/foto/5.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="../projectFinal/foto/1.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="../projectFinal/foto/2.png" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </section>
+    </form>
+    <!--carousel-->
+           
+    <?php 
+    //jika ada tombol simpan (tombil simpa di klik)
+    if (isset($_POST["login"]))
+    {
+        $email=$_POST["email"];
+        $password=$_POST["password"];
+
+        //lakukan query mengecek akun di tabel pelanggan di database
+        $ambil = $koneksi->query("SELECT * FROM pelanggan WHERE email_pelanggan='$email' AND password_pelanggan='$password'");
+
+        //menghitung akun yang terambil
+        $akunyangcocok=$ambil->num_rows;
+
+        //Jika 1 akun yang cocok, maka login sukses
+        if($akunyangcocok==1)
+        {
+            //Berhasil Login
+            //mendapatkan akun dalam bentuk array
+            $akun=$ambil->fetch_assoc();
+            //simpan di session pelanggan
+            $_SESSION["pelanggan"] = $akun;
+            echo "<script>alert('Login Success');</script>";
+            echo "<script>location='index.php'</script>";
+        }
+        else
+        {
+            //Gagal Login
+            echo "<script>alert('Login Failed, Check Your Email and Password');</script>";
+            echo "<script>location='login.php'</script>";
+        }
+
+    }
+    ?>
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
